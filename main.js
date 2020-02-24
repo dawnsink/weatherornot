85e904ab887bcedc2342078fe9072fc34d6e9ace
dawnsink <60200649+dawnsink@users.noreply.github.com>
2020-02-24 15:38:47 -0500

    fixed javascript

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 //api key = 3cd9030089f3276549b644a70aa6c30d
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
-
-function getWeather(){
-    $('.weatherResponse').html('');
-    var cityName = $('#cityName').val();
-    var apiCall='http://api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid={3cd9030089f3276549b644a70aa6c30d}';
-
-    $.getJSON(apiCall, weatherCallback);
-
-    function weatherCallback(weatherData){
-        var cityName = weatherData.name;
-        var country = weatherData.sys.country;
-        var description = weatherData.weather[0].description;
-        $('.weatherResponse').append("The weather in " + cityName + " " + country + " is currently " + description);
-    }
-
+async function getWeather(event){
+    const cityName=document.getElementById("cityName").value;
+    //console.log(cityName)
+    const url="http://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=3cd9030089f3276549b644a70aa6c30d";
+    //console.log(url)
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    //console.log(weatherData.weather[0].main)
+    
+    const weatherResult=document.getElementById("result");
+    weatherResult.style.textTransform = "lowercase";
+    weatherResult.innerHTML=`<p>The weather in ${cityName} is currently ${weatherData.weather[0].main}</p>`
 }
